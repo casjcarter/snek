ac97826b7b95ddfa968a772e1b2f414f9657e92e
Cassandra J Carter <casjcarter@gmail.com>
2020-11-14 13:31:17 -0500

    Added new coordinate data type to avoid having x and y values kept in separate variables.

--- snek.c
+++ snek.c
@@ -15,21 +15,22 @@
 
 /* structs */
 
+typedef struct {
+	int y;
+	int x;
+} coord;
+
 struct Player {
-	int locy;
-	int locx;
-	int diry;
-	int dirx;
+	coord loc;
+	coord dir;
+	coord* tail;
 	bool alive;
-	int* taily;
-	int* tailx;
 	int score;
 	bool snak_exists;
 };
 
 struct Snak {
-	int locy;
-	int locx;
+	coord loc;
 	bool exists; 
 };
 
@@ -60,9 +61,9 @@ bool is_some_char(WINDOW* win, int y, int x, char c) {
 	return false;
 }
 
-int* push(int* arr, int size, int value) {
+coord* push(coord* arr, int size, coord value) {
 	/* puts value in index 0 of arr, pushing all other values right */
-	for (int i = size-2; i >= 0; i--) *(arr+i+1) = *(arr+i);
+	for (int i = size-2; i >= 0; i--) arr[i+1] = arr[i];
 	arr[0] = value;
 	return arr;
 }
@@ -85,15 +86,14 @@ WINDOW* kill_win(WINDOW* win) {
 
 struct Player* init_player() {
 	struct Player* pl = (struct Player*)malloc(sizeof(struct Player));
-	pl -> locy = 1;
-	pl -> locx = 1;
-	pl -> diry = 0;
-	pl -> dirx = 0;
+
+	pl -> loc = (coord){1,1};
+	pl -> dir = (coord){0,0};
+	pl -> tail = (coord*)calloc(2, sizeof(coord));
 	pl -> alive = true;
-	pl -> taily = (int*)calloc(2, sizeof(int));
-	pl -> tailx = (int*)calloc(2, sizeof(int));
 	pl -> score = 0;
 	pl -> snak_exists = false;
+
 	return pl;
 }
 
@@ -109,30 +109,30 @@ bool is_walkable(WINDOW* win, int y, int x) {
 
 void move_player(WINDOW* win, struct Player* pl) {
 	/* Moves head, and pushes last location onto tail */
-	int to_y = pl -> locy + pl -> diry;
-	int to_x = pl -> locx + pl -> dirx;
+	int to_y = pl -> loc.y + pl -> dir.y;
+	int to_x = pl -> loc.x + pl -> dir.x;
+
 	if (is_walkable(win, to_y, to_x)) {
-		pl -> taily = push(pl -> taily, pl -> score + 1, pl -> locy);
-		pl -> tailx = push(pl -> tailx, pl -> score + 1, pl -> locx);
-		pl -> locy = to_y;
-		pl -> locx = to_x;
+		pl -> tail = push(pl -> tail, pl -> score + 1, pl -> loc);
+		pl -> loc = (coord){to_y, to_x};
 	} else {
 		pl -> alive = false;
 	}
 }
 
 void draw_player(WINDOW* win, struct Player* pl) {
+	/* draw tail */
 	for (int i = 0; i < pl -> score + 1; i++) {
-		if (pl -> taily[i] != 0 && pl -> tailx[i] != 0)
-			mvwaddch(win, pl -> taily[i], pl -> tailx[i], SNEK_TAIL);
+		if (pl -> tail[i].y != 0 && pl -> tail[i].x != 0)
+			mvwaddch(win, pl -> tail[i].y, pl -> tail[i].x, SNEK_TAIL);
 	}
-	mvwaddch(win, pl -> locy, pl -> locx, SNEK_HEAD);
+	/* draw head */
+	mvwaddch(win, pl -> loc.y, pl -> loc.x, SNEK_HEAD);
 }
 
 void grow_player(struct Player* pl) {
 	pl -> score++;
-	pl -> taily  = realloc(pl -> taily, (1 + pl -> score)*sizeof(int));
-	pl -> tailx  = realloc(pl -> tailx, (1 + pl -> score)*sizeof(int));
+	pl -> tail  = realloc(pl -> tail, (1 + pl -> score)*sizeof(coord));
 }
 
 /* snak related functions */
@@ -142,23 +142,25 @@ struct Snak* init_snak(WINDOW* win) {
 	struct Snak* sn = (struct Snak*)malloc(sizeof(struct Snak));
 	int y;
 	int x;
+
 	do {
 		y = (rand() % LEVEL_H) + 1;
 		x = (rand() % LEVEL_W) + 1;
 	} while (!is_some_char(win, y, x, FLOOR_CHAR));
+
 	mvwaddch(win, y, x, SNAK_CHAR);
-	sn -> locy = y;
-	sn -> locx = x;
+	sn -> loc = (coord){y,x};
 	sn -> exists = true;
+
 	return sn;
 }
 
 void draw_snak(WINDOW* win, struct Snak* sn) {
-	mvwaddch(win, sn -> locy, sn -> locx, SNAK_CHAR);
+	mvwaddch(win, sn -> loc.y, sn -> loc.x, SNAK_CHAR);
 }
 
 void collect_snak(struct Player* pl, struct Snak* sn) {
-	if (pl -> locy == sn -> locy && pl -> locx == sn -> locx) {
+	if (pl -> loc.x == sn -> loc.x	&& pl -> loc.y == sn -> loc.y) {
 		sn -> exists = false;
 		grow_player(pl);
 		free(sn);
@@ -186,31 +188,31 @@ void game_over() {
 void game_loop() {
 	WINDOW* game_win = init_win(LEVEL_H+2, LEVEL_W+2, 0, 0);
 	WINDOW* score_win = init_win(3, 7, LEVEL_H+2, 0);
+
 	struct Player* pl = init_player();
 	struct Snak* sn = init_snak(game_win);
+
 	char ch;
+
 	while (true) {
 		/* check player input */
 		ch = getch();
 		if (ch == 'q') break;
 		switch(ch) {
 			case 'w':
-				pl -> diry = -1;
-				pl -> dirx = 0;
+				pl -> dir = (coord){-1,0};
 				break;
 			case 's':
-				pl -> diry = 1;
-				pl -> dirx = 0;
+				pl -> dir = (coord){1,0};
 				break;
 			case 'a':
-				pl -> diry = 0;
-				pl -> dirx = -1;
+				pl -> dir = (coord){0,-1};
 				break;
 			case 'd':
-				pl -> diry = 0;
-				pl -> dirx = 1;
+				pl -> dir = (coord){0,1};
 				break;
 		}
+
 		werase(game_win);
 
 		wborder(game_win, '#', '#', '#', '#', '#', '#', '#', '#');
@@ -223,15 +225,16 @@ void game_loop() {
 		wrefresh(score_win);
 
 		move_player(game_win, pl);
+
 		if (!(pl -> alive)) break;
 		if (!(sn -> exists)) sn = init_snak(game_win);
+
 		collect_snak(pl, sn);
 
 		napms(1000/FRAME_RATE);
 	}
 	/* free player tail memory */
-	free(pl -> taily);
-	free(pl -> tailx);
+	free(pl -> tail);
 	free(pl);
 	pl = NULL;
 
